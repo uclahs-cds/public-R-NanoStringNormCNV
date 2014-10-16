1115e97ce4b7e966d355b78497bcb6694bf5be91
cyao <cyao@noemail.ucla.edu>
2014-10-16 20:49:15 +0000

    NanoStringNormCNV_0.0.3: - major changes to call.copy.number.state in cases when only one reference sample is supplied; made sure that all samples were divided by reference sample before the reference sample gets divided - added proper annotation columns for the return table
    git-svn-id: https://code.boutroslab.oicr.on.ca/svn/BoutrosLab@35702 aa8168d8-c5e5-4185-88a0-f3e152ed764a

--- R/call.copy.number.state.R
+++ R/call.copy.number.state.R
@@ -12,117 +12,136 @@ call.copy.number.state <- function (input, reference, per.chip = FALSE, chip.inf
 	# remove y.genes from input	  TO DO: get gender info to determine whether this should be done!
 	input <- input[!input$Name %in% y.genes,];
 
-	# assess input file and create an empty data-frame for data storage
-	if (any(header.names %in% colnames(input))) {
+	# create empty data-frame to store data
+	out.cna <- input;
+	to.keep <- colnames(input);
 
-			# check if these three columns are in the header
-			out.cna   <- input;
-			which.cna <- colnames(input)[!colnames(input) %in% header.names];
+	# if header is present remove headers from to.keep
+	if (any(header.names %in% to.keep)) { to.keep <- to.keep[!to.keep %in% header.names]; }
 
-			# make the rest of the data NA and only keeping the headers
-			out.cna[,which.cna] <- NA;
-			}
-	else{
-		out.cna <- input;
-		out.cna <- NA;
-		}
+	# make out.cna all NAs for data storage
+	out.cna <- out.cna[,to.keep];
+	out.cna[,to.keep] <- NA;
+
+	# remove to.keep
+	rm(to.keep);
 
 	# see if user asks for per.chip
-	if (per.chip) {
-			n.chip     <- length(unique(chip.info$Chip));
-			ref.backup <- reference;
-			}
+	if (per.chip)       { n.chip <- length(unique(chip.info$Chip)); }
+	else if (!per.chip) { n.chip <- 1;                              }
 
-	else if (!per.chip) { n.chip <- 1; }
+	# define samples.to.loop here
+	samples.to.loop <- colnames(out.cna)[!colnames(out.cna) %in% reference];
 
 	# loop over n.chip
 	for (perm.i in 1:n.chip) {
 
-			# if per.chip is TRUE
-			if (per.chip) {
+		# define a tmp.ref every time before the start of a new iteration
+		tmp.ref <- reference;
 
-					# get the reference for chip specifically
-					this.chip <- paste0('Chip ', perm.i);
-					to.keep   <- chip.info$Chip %in% this.chip & chip.info$SampleID %in% ref.backup;
-					reference <- chip.info$SampleID[to.keep];
+		# if per.chip is TRUE
+		if (per.chip) {
 
-					# when per chip is requested, but there are no reference samples on the chip, use pooled references then
-					if(length(reference) < 1){
-						next;
-#						to.keep   <- chip.info$SampleID %in% ref.backup;
-#						reference <- chip.info$SampleID[to.keep];
-						}
-					which.cna <- chip.info$SampleID[this.chip == chip.info$Chip];
-					which.cna <- c(which.cna[!which.cna %in% reference], reference);
-					}
+			# get the tmp.ref for chip specifically
+			this.chip <- paste0('Chip ', perm.i);
+			tmp.ref <- chip.info$SampleID[this.chip == chip.info$Chip & reference == chip.info$SampleID];
 
-			# if length of reference is greater than 1, take the average of the reference
-			if (length(reference) > 1) {
+			# when per chip is requested, but there are no ref samples on the chip, use pooled refs then
+			if (length(tmp.ref) < 1) { next; }
 
-					# take the average and create a new column in input as avg.ref
-					input$avg.ref <- apply(X = input[,reference], MARGIN = 1, FUN = mean);
-
-					# and change reference to 'avg.ref'
-					reference <- 'avg.ref';
-					}
-
-			# loop over each sample
-			for (this.sample in which.cna) {
-					# divide each test samples probe value by corresponding probes in the reference samples
-				if(any(input[,reference] == 0)){
-						input[,reference][input[,reference] == 0] <- 1;
-						}
-					tmp.ratio <- input[,this.sample] / input[,reference];
-
-					# multiply ratio by multiplication factor
-					out.cna[,this.sample] <- tmp.ratio * multi.factor;
-					}
+			samples.to.loop <- chip.info$SampleID[this.chip == chip.info$Chip];
+			samples.to.loop <- samples.to.loop[!samples.to.loop %in% tmp.ref];
 			}
+
+		# if length of tmp.ref is greater than 1, take the average of the tmp.ref
+		if (length(tmp.ref) > 1) {
+
+			# take the average and create a new column in input as avg.ref
+			input$avg.ref <- apply(X = input[,tmp.ref], MARGIN = 1, FUN = mean);
+
+			# and change tmp.ref to 'avg.ref'
+			tmp.ref <- 'avg.ref';
+			}
+
+		# loop over each sample first and then do the processing for reference samples!
+		for (this.sample in samples.to.loop) {
+
+			# divide each test samples probe value by corresponding probes in the ref samples
+			if (any(0 == input[,tmp.ref])) { input[,tmp.ref][0 == input[,tmp.ref]] <- 1; }
+			tmp.ratio <- input[,this.sample] / input[,tmp.ref];
+
+			# multiply ratio by multiplication factor
+			out.cna[,this.sample] <- tmp.ratio * multi.factor;
+			}
+
+		# do the above separately for reference samples
+		if (tmp.ref != 'avg.ref') {
+
+			tmp.ratio <- input[,tmp.ref] / input[,tmp.ref];
+			out.cna[,tmp.ref] <- tmp.ratio * multi.factor;
+			}
+		}
 
 	# if user specified to.round then do the following
 	if (to.round) {
 
-			# round the numbers to the nearest integer ** rounding 1.5 and 2.5 to 2 and 3 respectively **
-			which.cna     <- colnames(out.cna)[!colnames(out.cna) %in% header.names];
-			which.n <- which(colnames(out.cna) %in% which.cna);
-			# need to exclude columns with all NAs (occurs when perchip=T and there are no reference samples on that chip!)
-			if(any(apply(out.cna[, which.n, drop = FALSE], 2, function(f) all(is.na(f))))){
-				all.na <- which(apply(out.cna[, which.n, drop = FALSE], 2, function(f) all(is.na(f))));
-				which.n <- which.n[-all.na];
-				out.cna[,which.n] <- round(out.cna[,which.n, drop = FALSE], digits = 1);
-				out.cna.round <- out.cna[, which.cna, drop = FALSE];
-			}else{
-				out.cna.round <- round(out.cna[,which.cna, drop = FALSE], digits = 1);
-				}
+		# round the numbers to the nearest integer ** rounding 1.5 and 2.5 to 2 and 3 respectively **
+		which.cna <- colnames(out.cna)[!colnames(out.cna) %in% header.names];
+		which.n   <- which(colnames(out.cna) %in% which.cna);
 
-			# loop over each gene
-			for (row.ind in 1:nrow(out.cna.round)) {
+		# pull below into a separate object
+		na.counts <- apply(
+			X = out.cna[,which.n, drop = FALSE],
+			MARGIN = 2,
+			FUN = function(f) { all(is.na(f)) }
+			);
 
-					# get a list of genes with the following criteria (should 0.5 CN be classified as 1?)
-					which.0 <- which(out.cna.round[row.ind,] <= 0.4);
-					which.1 <- which(out.cna.round[row.ind,] >= 0.6 & out.cna.round[row.ind,] <= 1.4);
-					which.2 <- which(out.cna.round[row.ind,] >= 1.6 & out.cna.round[row.ind,] <= 2.4);
-					which.3 <- which(out.cna.round[row.ind,] >= 2.6 & out.cna.round[row.ind,] <= 3.4);
-					which.4 <- which(out.cna.round[row.ind,] >= 3.6);
-	
-					# convert value to the nearest integer
-					out.cna.round[row.ind, which.0] <- 0;
-					out.cna.round[row.ind, which.1] <- 1;
-					out.cna.round[row.ind, which.2] <- 2;
-					out.cna.round[row.ind, which.3] <- 3;
-					out.cna.round[row.ind, which.4] <- 4;
-					}
-
-			# add the probe information back to out.cna.round
-			out.cna.round <- cbind(
-					out.cna[,colnames(input) %in% header.names, drop = FALSE],
-					out.cna.round
-					);
-
-			# return out.cna.round
-			return(out.cna.round);
+		# need to exclude columns with all NAs (occurs when perchip=T and there are no reference samples on that chip!)
+		if (any(na.counts)) {
+			all.na <- which(na.counts);
+			which.n <- which.n[-all.na];
+			out.cna[,which.n] <- round(out.cna[,which.n, drop = FALSE], digits = 1);
+			out.cna.round <- out.cna[, which.cna, drop = FALSE];
 			}
 
+		else { out.cna.round <- round(out.cna[,which.cna, drop = FALSE], digits = 1); }
+
+		# loop over each gene
+		for (row.ind in 1:nrow(out.cna.round)) {
+
+			# get a list of genes with the following criteria (should 0.5 CN be classified as 1?)
+			which.0 <- which(out.cna.round[row.ind,] <= 0.4);
+			which.1 <- which(out.cna.round[row.ind,] >= 0.6 & out.cna.round[row.ind,] <= 1.4);
+			which.2 <- which(out.cna.round[row.ind,] >= 1.6 & out.cna.round[row.ind,] <= 2.4);
+			which.3 <- which(out.cna.round[row.ind,] >= 2.6 & out.cna.round[row.ind,] <= 3.4);
+			which.4 <- which(out.cna.round[row.ind,] >= 3.6);
+
+			# convert value to the nearest integer
+			out.cna.round[row.ind, which.0] <- 0;
+			out.cna.round[row.ind, which.1] <- 1;
+			out.cna.round[row.ind, which.2] <- 2;
+			out.cna.round[row.ind, which.3] <- 3;
+			out.cna.round[row.ind, which.4] <- 4;
+			}
+
+		# add the probe information back to out.cna.round
+		out.cna.round <- cbind(
+			input[,colnames(input)[colnames(input) %in% header.names], drop = FALSE],
+			out.cna.round
+			);
+
+		# return out.cna.round
+		return(out.cna.round);
+		}
+
 	# else return out.cna
-	else { return(out.cna); }
+	else {
+
+		return(
+			cbind(
+				input[,colnames(input)[colnames(input) %in% header.names], drop = FALSE],
+				out.cna
+				)
+			);
+		}
 	}
