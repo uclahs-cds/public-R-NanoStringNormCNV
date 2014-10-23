8868338aec881b934726ccaf481a6efa709d7caf
cyao <cyao@noemail.ucla.edu>
2014-10-23 22:32:53 +0000

    NanoStringNorm_0.08: - minor tweak to default plotting parameters within make.cna.heatmap; also brought out create.heatmap parameters so users can specify that within make.cna.heatmap
    git-svn-id: https://code.boutroslab.oicr.on.ca/svn/BoutrosLab@36025 aa8168d8-c5e5-4185-88a0-f3e152ed764a

--- R/make.cna.heatmap.R
+++ R/make.cna.heatmap.R
@@ -1,7 +1,14 @@
 
-make.cna.heatmap <- function(nano.cnas, fname.stem, covs.rows = NULL, covs.cols = NULL, covs.legend = NULL, rounded = FALSE, clust.dim = 'both'){
+make.cna.heatmap <- function(
+	nano.cnas, fname.stem, covs.rows = NULL, covs.cols = NULL, covs.legend = NULL,
+	rounded = FALSE, clust.dim = 'both', ...
+	) {
 
+	# define parameters up front
 	dist.method <- ifelse(rounded, 'jaccard', 'correlation');
+	plot.at <- seq(floor(min(nano.cnas, na.rm = TRUE)), ceiling(max(nano.cnas, na.rm = TRUE)), 0.1);
+	plot.seq <- seq(min(plot.at), max(plot.at), 5);
+
 	create.heatmap(
 		x = nano.cnas,
 		filename = generate.filename(fname.stem, 'cna_heatmap', 'png'),
@@ -14,14 +21,15 @@ make.cna.heatmap <- function(nano.cnas, fname.stem, covs.rows = NULL, covs.cols 
 		rows.distance.method = dist.method,
 		cols.distance.method = dist.method,
 		colour.centering.value = 3,
-		at = seq(1,max(nano.cnas),by = 0.1),
-		colourkey.labels = c(0,2,4),
-		colourkey.labels.at = c(1,3,5),
+		at = plot.at,
+		colourkey.labels = if (rounded) { c(0,2,4) } else { plot.seq },
+		colourkey.labels.at = if (rounded) { c(1,3,5) } else { plot.seq },
 		covariates = covs.rows,
 		covariates.top = covs.cols,
 		covariate.legends = covs.legend,
 		colour.scheme = c('blue', 'white', 'red'),
-		resolution = 600
+		resolution = 600,
+		...
 		);
-
 	}
+
