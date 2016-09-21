286ed5e7316a6bb3c739e15e72d3db97ab5fe979
dsendorek <dsendorek@noemail.ucla.edu>
2016-09-21 19:42:31 +0000

    fixed xaxis labelling, input processing, plot padding
    git-svn-id: https://code.boutroslab.oicr.on.ca/svn/BoutrosLab@57251 aa8168d8-c5e5-4185-88a0-f3e152ed764a

--- R/make.positive.control.plot.R
+++ R/make.positive.control.plot.R
@@ -1,7 +1,9 @@
 
 make.positive.control.plot <- function(corr.df, fname.stem){		### TO DO: Add in covariates and allow clustering
 	# order matrix according to correlation
-	corr.df <- corr.df[order(corr.df[,1]) ,];
+	corr.df <- corr.df[order(corr.df[,1]), 1, drop = FALSE];
+	rownames(corr.df) <- NULL;
+	
 	create.heatmap(
 		x = as.matrix(corr.df),
 		filename = BoutrosLab.utilities::generate.filename(fname.stem, 'full_range', 'png'),
@@ -9,14 +11,16 @@ make.positive.control.plot <- function(corr.df, fname.stem){		### TO DO: Add in 
 		main.cex = 2,
 		cluster.dimensions = 'none',
 		height = 2,
-		scale.data=F,
+		scale.data = FALSE,
 		xat = seq(0, nrow(corr.df), by = 20),
-		xaxis.lab = rownames(corr.df)[seq(20,nrow(corr.df), by = 20)],
+		xaxis.lab = c(0, rownames(corr.df)[seq(20, nrow(corr.df), by = 20)]),
 		xlab.label = 'Samples',
-		at = seq(0,1,by =0.01),
+		at = seq(0, 1, by = 0.01),
 		colourkey.cex = 1.5,
-		resolution = 600
+		resolution = 600,
+		right.padding = 2
 		);
+	
 	create.heatmap(
 		x = as.matrix(corr.df),
 		filename = BoutrosLab.utilities::generate.filename(fname.stem, 'zoomed', 'png'),
@@ -24,11 +28,13 @@ make.positive.control.plot <- function(corr.df, fname.stem){		### TO DO: Add in 
 		main.cex = 2,
 		cluster.dimensions = 'none',
 		height = 2,
-		scale.data=F,
+		scale.data = FALSE,
 		xat = seq(0, nrow(corr.df), by = 20),
-		xaxis.lab = rownames(corr.df)[seq(20,nrow(corr.df), by = 20)],
+		xaxis.lab = c(0, rownames(corr.df)[seq(20, nrow(corr.df), by = 20)]),
 		xlab.label = 'Samples',
 		colourkey.cex = 1.5,
-		resolution = 600
+		resolution = 600,
+		right.padding = 2
 		);
 	}
+
