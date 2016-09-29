f560166af2a9a56782270c519236be22b78ca744
dsendorek <dsendorek@noemail.ucla.edu>
2016-09-29 17:55:24 +0000

    NanoStringNormCNV v0.0.17 - removing BL.utilities.copynumber dependency - minor cleanup in get.ari and score.runs - fixed man usage for call.cna.states
    git-svn-id: https://code.boutroslab.oicr.on.ca/svn/BoutrosLab@57518 aa8168d8-c5e5-4185-88a0-f3e152ed764a

--- R/get.ari.R
+++ R/get.ari.R
@@ -1,19 +1,18 @@
 
 get.ari <- function(data, feature, discrete.data = FALSE){
-	# if data isn't continuous, use Jaccard and Ward combo
-	if(discrete.data){
-		dist.matrix <- dist(x=data, method = 'jaccard');
-		corr.hc <- hclust(dist.matrix, method = 'ward');
-		predicted.feature <- cutree(corr.hc, length(unique(feature)));
-		ari <- adjustedRandIndex(predicted.feature, factor(feature, levels = unique(feature)));
-		}
-	else{
-		cor.matrix <- cor(data, use = 'all.obs', method = 'pearson');
-		cor.dist <- as.dist(1-cor(cor.matrix, use = 'pairwise', method = 'pearson'));
-		cor.dendo <- hclust(cor.dist, method = 'complete'); # 
-		feature <- factor(feature, levels = unique(feature));
-		predicted.feature <- cutree(cor.dendo, length(levels(feature)));
-		ari <- adjustedRandIndex(predicted.feature, feature);
+	# if data isn't continuous, use Jaccard and Ward in combination
+	if (discrete.data) {
+		dist.matrix <- dist(x = data, method = 'jaccard');
+		corr.hc 	<- hclust(dist.matrix, method = 'ward.D');
+		prediction 	<- cutree(corr.hc, length(unique(feature)));
+		ari			<- adjustedRandIndex(prediction, factor(feature, levels = unique(feature)));
+	} else {
+		corr.matrix <- cor(data, use = 'all.obs', method = 'pearson');
+		corr.dist 	<- as.dist(1 - cor(corr.matrix, use = 'pairwise', method = 'pearson'));
+		corr.dendo 	<- hclust(corr.dist, method = 'complete');
+		feature 	<- factor(feature, levels = unique(feature));
+		prediction 	<- cutree(corr.dendo, length(levels(feature)));
+		ari 		<- adjustedRandIndex(prediction, feature);
 		}
 
 	return(ari);
