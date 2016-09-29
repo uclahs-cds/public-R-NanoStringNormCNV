b2c6a4e1f878141343897f8fd058e98947ca85a0
dsendorek <dsendorek@noemail.ucla.edu>
2016-09-29 20:15:03 +0000

    removing BoutrosLab.utilities (generate.filename) dependency; removing fname.stem arg; eliminated warning messages; changed input from matrix to vector; xaxis range is now probe number-dependent instead of hard coded
    git-svn-id: https://code.boutroslab.oicr.on.ca/svn/BoutrosLab@57539 aa8168d8-c5e5-4185-88a0-f3e152ed764a

--- trunk/NanoStringNormCNV/R/make.positive.control.plot.R
+++ trunk/NanoStringNormCNV/R/make.positive.control.plot.R
@@ -1,40 +1,50 @@
 
-make.positive.control.plot <- function(corr.df, fname.stem){		### TO DO: Add in covariates and allow clustering
-	# order matrix according to correlation
-	corr.df <- corr.df[order(corr.df[,1]), 1, drop = FALSE];
-	rownames(corr.df) <- NULL;
+make.positive.control.plot <- function(correlations) {		### TO DO: Add in covariates and allow clustering
+	# order correlation by size
+	correlations <- sort(correlations);
+
+	# set up x-axis
+	xaxis.range <- pretty(seq(0, length(correlations)), n = 4);
+	xaxis.range <- xaxis.range[xaxis.range < length(correlations)];
 	
 	BoutrosLab.plotting.general::create.heatmap(
-		x = as.matrix(corr.df),
-		filename = BoutrosLab.utilities::generate.filename(fname.stem, 'full_range', 'png'),
-		main = 'Positive Probe Correlations',
-		main.cex = 2,
+		x = as.matrix(correlations),
+		filename = paste0(Sys.Date(), '_positive-control-correlations_full-range.tiff'),
 		cluster.dimensions = 'none',
-		height = 2,
+		main = 'Positive Probe Correlations',
+		main.cex = 1.8,
 		scale.data = FALSE,
-		xat = seq(0, nrow(corr.df), by = 20),
-		xaxis.lab = c(0, rownames(corr.df)[seq(20, nrow(corr.df), by = 20)]),
 		xlab.label = 'Samples',
-		at = seq(0, 1, by = 0.01),
+		xlab.cex = 1.2,
+		xat = xaxis.range,
+		xaxis.lab = xaxis.range,
+		yaxis.tck = 0,
+		colour.scheme = c('red', 'white', 'blue'),
+		colour.centering.value = 0.5,
 		colourkey.cex = 1.5,
-		resolution = 600,
-		right.padding = 2
+		at = seq(0, 1, by = 0.01),
+		right.padding = 2,
+		height = 2,
+		resolution = 600
 		);
 	
 	BoutrosLab.plotting.general::create.heatmap(
-		x = as.matrix(corr.df),
-		filename = BoutrosLab.utilities::generate.filename(fname.stem, 'zoomed', 'png'),
-		main = 'Positive Probe Correlations',
-		main.cex = 2,
+		x = as.matrix(correlations),
+		filename = paste0(Sys.Date(), '_positive-control-correlations_zoomed-in.tiff'),
 		cluster.dimensions = 'none',
-		height = 2,
+		main = 'Positive Probe Correlations',
+		main.cex = 1.8,
 		scale.data = FALSE,
-		xat = seq(0, nrow(corr.df), by = 20),
-		xaxis.lab = c(0, rownames(corr.df)[seq(20, nrow(corr.df), by = 20)]),
 		xlab.label = 'Samples',
+		xlab.cex = 1.2,
+		xat = xaxis.range,
+		xaxis.lab = xaxis.range,
+		yaxis.tck = 0,
+		colour.scheme = c('white','blue'),
 		colourkey.cex = 1.5,
-		resolution = 600,
-		right.padding = 2
+		right.padding = 2,
+		height = 2,
+		resolution = 600
 		);
 	}
 
