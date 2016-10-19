f19a1e7afa660df2af5e51b6396ee394dfd25b69
dsendorek <dsendorek@noemail.ucla.edu>
2016-10-19 18:32:00 +0000

    changed var name to match rest of pkg; improved man file
    git-svn-id: https://code.boutroslab.oicr.on.ca/svn/BoutrosLab@58053 aa8168d8-c5e5-4185-88a0-f3e152ed764a

--- trunk/NanoStringNormCNV/R/positive.control.norm.R
+++ trunk/NanoStringNormCNV/R/positive.control.norm.R
@@ -1,8 +1,8 @@
 
-positive.control.norm <- function(nano.df){
-	#--- positive control correlation ----------------------------------------------------------------#
-	target.conc <- c(128, 32, 8, 2, 0.5, 0.125);	# According to the NS nCounter CNV manaual pg 7
-	target.conc <- target.conc*171.23 + 214.12;
+positive.control.norm <- function(nano.df) {
+	# positive control correlation according to the NS nCounter CNV manaual pg 7
+	target.conc <- c(128, 32, 8, 2, 0.5, 0.125);
+	target.conc <- target.conc * 171.23 + 214.12;
 
 	# only keep the rows pertaining to positive code class
 	nano.pos <- nano.df['Positive' == nano.df$CodeClass,];
@@ -10,7 +10,7 @@ positive.control.norm <- function(nano.df){
 
 	# initiate object to store R2
 	R2.results <- data.frame(
-        SampleName = colnames(nano.pos)[4:ncol(nano.pos)],
+        SampleID = colnames(nano.pos)[4:ncol(nano.pos)],
         R2 = NA,
         stringsAsFactors = FALSE
         );
@@ -18,7 +18,7 @@ positive.control.norm <- function(nano.df){
 	# for each sample, calculate R^2 and create scatterplot
 	for (col.ind in 4:ncol(nano.pos)) {
 		# get R^2 measure
-		R2.results$R2[colnames(nano.pos)[col.ind] == R2.results$SampleName] <- cor(
+		R2.results$R2[colnames(nano.pos)[col.ind] == R2.results$SampleID] <- cor(
 			nano.pos[,col.ind], target.conc
 			)^2;
 		}
