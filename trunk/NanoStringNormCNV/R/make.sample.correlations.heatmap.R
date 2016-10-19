3e55cc3a659ca3c22fe307542c8eecab8540d115
dsendorek <dsendorek@noemail.ucla.edu>
2016-10-19 19:23:53 +0000

    added auto covs and legend generation; other minor improvements to plot; expanded man
    git-svn-id: https://code.boutroslab.oicr.on.ca/svn/BoutrosLab@58057 aa8168d8-c5e5-4185-88a0-f3e152ed764a

--- trunk/NanoStringNormCNV/R/make.sample.correlations.heatmap.R
+++ trunk/NanoStringNormCNV/R/make.sample.correlations.heatmap.R
@@ -1,18 +1,50 @@
 
-make.sample.correlations.heatmap <- function(data, cor.method = 'pearson', fname.stem = NULL, covs = NULL, covs.legend = NULL) {
-	# get the inter-array correlations and plot
-	correlations <- cor(x = data, use = 'all.obs', method = cor.method);
+make.sample.correlations.heatmap <- function(nano.counts, cor.method = 'pearson', fname.stem = NULL, covs = NULL) {
+	# check and set up covariates
+	if (!is.null(covs)) {
+		# check completeness and order
+		if (! all( colnames(nano.counts) %in% covs$SampleID )) {
+			stop("Must provide covariate information for every sample!");
+		} else {
+			c.row <- c.row[match(colnames(nano.counts), c.row$SampleID),];
+			}
 
+		covs <- covs[, !(names(covs) == 'SampleID'), drop = FALSE];
+		rownames(covs) <- NULL;
+
+		# create covariate object
+		cov.obj <- NanoStringNormCNV::generate.plot.covariates(cov.info = covs);
+	} else {
+		cov.obj <- NULL;
+		}
+
+	# set up legend
+	if (!is.null(covs)) {
+		covs.legend <- NanoStringNormCNV::generate.plot.legend(cov.info = as.list(covs));
+	} else {
+		covs.legend <- NULL;
+		}
+
+	# set up file name
 	if (!is.null(fname.stem)) { fname.stem <- paste0("_", fname.stem); }
 
+	# get the inter-array correlations
+	correlations <- cor(x = nano.counts, use = 'all.obs', method = cor.method);
+
+	# plot
+	cov.border <- list(col = 'black', lwd = 1.5);
 	BoutrosLab.plotting.general::create.heatmap(
 		x = correlations,
 		filename = paste0(Sys.Date(), fname.stem, '_inter-sample-correlation_heatmap.tiff'),
 		clustering.method = 'complete',
 		plot.dendrograms = 'both',
 		xaxis.rot = 90,
-		covariates = covs,
-		covariates.top = covs,
+		xaxis.tck = 0,
+		yaxis.tck = 0,
+		covariates = cov.obj,
+		covariates.top.grid.border = cov.border,
+		covariates.top = cov.obj,
+		covariates.grid.border = cov.border,
 		covariate.legends = covs.legend,
 		legend.title.just = 'left',
 		legend.title.fontface = 'bold',
