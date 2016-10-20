f129400c09c3e330e782f51afe09ac13f208dfd3
dsendorek <dsendorek@noemail.ucla.edu>
2016-10-20 19:54:54 +0000

    modified output to mimic general plotting function input
    git-svn-id: https://code.boutroslab.oicr.on.ca/svn/BoutrosLab@58081 aa8168d8-c5e5-4185-88a0-f3e152ed764a

--- R/positive.control.norm.R
+++ R/positive.control.norm.R
@@ -9,22 +9,23 @@ positive.control.norm <- function(nano.df) {
 	nano.pos <- nano.pos[with(nano.pos, order(Name)),];
 
 	# initiate object to store R2
-	R2.results <- data.frame(
-        SampleID = colnames(nano.pos)[4:ncol(nano.pos)],
-        R2 = NA,
-        stringsAsFactors = FALSE
+	R2.results <- matrix(
+		ncol = ncol(nano.pos) - 3,
+		nrow = 1,
+		dimnames = list("R2", colnames(nano.pos)[4:ncol(nano.pos)])
         );
+	R2.results <- as.data.frame(R2.results);
 
 	# for each sample, calculate R^2 and create scatterplot
 	for (col.ind in 4:ncol(nano.pos)) {
 		# get R^2 measure
-		R2.results$R2[colnames(nano.pos)[col.ind] == R2.results$SampleID] <- cor(
+		R2.results[1, colnames(nano.pos)[col.ind] == colnames(R2.results)] <- cor(
 			nano.pos[,col.ind], target.conc
 			)^2;
 		}
 
 	# throw a warning if not all positive controls have R^2 > 0.95
-	if (!all(R2.results$R2 >= 0.95)) { warning('Not all probes have R^2 > 0.95'); }
+	if (!all(R2.results[1,] >= 0.95)) { warning('Not all probes have R^2 > 0.95'); }
 	
 	return(R2.results);
 	}
