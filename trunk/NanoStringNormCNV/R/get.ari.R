72353a75527f3168e79d5193fac65c2090418df5
dsendorek <dsendorek@noemail.ucla.edu>
2016-10-24 22:57:24 +0000

    setting default to discrete data (may remove continuous data in future as no longer applicable in score.runs); transposing dist.matrix input to make sample (not gene) predictions; updated man
    git-svn-id: https://code.boutroslab.oicr.on.ca/svn/BoutrosLab@58196 aa8168d8-c5e5-4185-88a0-f3e152ed764a

--- trunk/NanoStringNormCNV/R/get.ari.R
+++ trunk/NanoStringNormCNV/R/get.ari.R
@@ -1,12 +1,12 @@
-
-get.ari <- function(data, feature, discrete.data = FALSE){
-	# if data isn't continuous, use Jaccard and Ward in combination
+get.ari <- function(data, feature, discrete.data = TRUE){
 	if (discrete.data) {
-		dist.matrix <- dist(x = data, method = 'jaccard');
+		# if data is discrete, use Jaccard and Ward
+		dist.matrix <- dist(x = t(data), method = 'jaccard');
 		corr.hc 	<- hclust(dist.matrix, method = 'ward.D');
 		prediction 	<- cutree(corr.hc, length(unique(feature)));
 		ari			<- adjustedRandIndex(prediction, factor(feature, levels = unique(feature)));
 	} else {
+		# if data is continuous, use Pearson and complete clustering
 		corr.matrix <- cor(data, use = 'all.obs', method = 'pearson');
 		corr.dist 	<- as.dist(1 - cor(corr.matrix, use = 'pairwise', method = 'pearson'));
 		corr.dendo 	<- hclust(corr.dist, method = 'complete');
