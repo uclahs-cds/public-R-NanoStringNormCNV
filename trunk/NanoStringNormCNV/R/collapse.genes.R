8a94a3bd3c66b13b93a1a4bc0372aa458cf25c4d
dsendorek <dsendorek@noemail.ucla.edu>
2016-10-25 17:58:20 +0000

    changed input/output formats to minimize re-formatting pre/post function
    git-svn-id: https://code.boutroslab.oicr.on.ca/svn/BoutrosLab@58207 aa8168d8-c5e5-4185-88a0-f3e152ed764a

--- trunk/NanoStringNormCNV/R/collapse.genes.R
+++ trunk/NanoStringNormCNV/R/collapse.genes.R
@@ -1,4 +1,3 @@
-
 collapse.genes <- function(nano.df){
 	# generate nano.df.aggr gene-level data
 	nano.df.gene <- aggregate(
@@ -9,6 +8,16 @@ collapse.genes <- function(nano.df){
 
 	# rename the Group.1 column
 	colnames(nano.df.gene)['Group.1' == colnames(nano.df.gene)] <- 'Name';
+
+	# add gene annotation 
+	matching.inds <- unlist(lapply(nano.df.gene$Name, function(f) which(nano.df$Accession == f)[1]));
+	nano.df.gene  <- cbind(
+		CodeClass = nano.df[matching.inds, "CodeClass"],
+		Name = nano.df.gene[, "Name"],
+		Accession = nano.df[matching.inds, "Accession"],
+		nano.df.gene[, !colnames(nano.df.gene) == "Name"]
+		);
+	rownames(nano.df.gene) <- nano.df.gene$Name;
 
 	return(nano.df.gene);
 	}
