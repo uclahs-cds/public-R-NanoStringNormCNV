4c5ee03764953cd1e4f901c656a23c52ba88aba1
dsendorek <dsendorek@noemail.ucla.edu>
2016-12-21 22:27:27 +0000

    standardized var names; modified code to allow user to provide either count or CNA info if both are not available; clarified doc
    git-svn-id: https://code.boutroslab.oicr.on.ca/svn/BoutrosLab@59823 aa8168d8-c5e5-4185-88a0-f3e152ed764a

--- trunk/NanoStringNormCNV/R/evaluate.replicates.R
+++ trunk/NanoStringNormCNV/R/evaluate.replicates.R
@@ -1,48 +1,63 @@
-evaluate.replicates <- function(normalized.data, phenodata, cnas) {
+evaluate.replicates <- function(phenodata, normalized.data = NULL, cna.rounded = NULL) {
 	# get samples with replicates
 	pheno.reps <- phenodata[which(phenodata$HasReplicate == 1),];
 
-	nano.reps <- normalized.data[, colnames(normalized.data) %in% pheno.reps$SampleID];
-	cna.reps  <- cnas[, colnames(cnas) %in% pheno.reps$SampleID];
+	count.reps <- normalized.data[, colnames(normalized.data) %in% pheno.reps$SampleID];
+	cna.reps   <- cna.rounded[, colnames(cna.rounded) %in% pheno.reps$SampleID];
 
-	pheno.reps.nano <- pheno.reps[pheno.reps$SampleID %in% colnames(nano.reps),];
-	pheno.reps.cnas <- pheno.reps[pheno.reps$SampleID %in% colnames(cna.reps),];
+	pheno.reps.count <- pheno.reps[pheno.reps$SampleID %in% colnames(count.reps),];
+	pheno.reps.cnas  <- pheno.reps[pheno.reps$SampleID %in% colnames(cna.reps),];
 
-	# checks
-	if (!(ncol(nano.reps) > 0)) {
-		stop("Cannot proceed with evaluation as no replicates were identified in normalized count data!");
+	# check for replicates; if none, set output to NULL
+	if (!is.null(count.reps) && !(ncol(count.reps) > 0)) {
+		flog.warn("Cannot evaluate using normalized count data as no replicates were identified in dataset!");
+		var.matrix <- count.reps <- pheno.reps.count <- NULL;
+	} else if (is.null(count.reps)) {
+		var.matrix <- count.reps <- pheno.reps.count <- NULL;
 		}
-	if (!(ncol(cna.reps) > 0)) {
-		stop("Cannot proceed with evaluation as no replicates were identified in CNA call data!");
+
+	if (!is.null(cna.reps) && !(ncol(cna.reps) > 0)) {
+		flog.warn("Cannot evaluate using CNA data as no replicates were identified in dataset!");
+		conc.matrix <- conc.summary <- cna.reps <- pheno.reps.cnas <- NULL;
+	} else if (is.null(cna.reps)) {
+		conc.matrix <- conc.summary <- cna.reps <- pheno.reps.cnas <- NULL;
 		}
 
-	# order according to samples
-	pheno.reps.nano <- pheno.reps.nano[order(pheno.reps.nano$SampleID),];
-	nano.reps <- nano.reps[, pheno.reps.nano$SampleID];
+	# order samples
+	if (!is.null(count.reps)) {
+		pheno.reps.count <- pheno.reps.count[order(pheno.reps.count$SampleID),];
+		count.reps <- count.reps[, pheno.reps.count$SampleID];
+		}
 
-	pheno.reps.cnas <- pheno.reps.cnas[order(pheno.reps.cnas$SampleID),];
-	cna.reps <- cna.reps[, pheno.reps.cnas$SampleID];
+	if (!is.null(cna.reps)) {
+		pheno.reps.cnas <- pheno.reps.cnas[order(pheno.reps.cnas$SampleID),];
+		cna.reps <- cna.reps[, pheno.reps.cnas$SampleID];
+		}
 
-	# check count variance
-	var.matrix <- NanoStringNormCNV::calculate.replicate.variance(
-		norm.data.reps = nano.reps,
-		phenodata.reps = pheno.reps.nano
-		);
+	# calculate count variance
+	if (!is.null(count.reps)) {
+		var.matrix <- NanoStringNormCNV::calculate.replicate.variance(
+			norm.data.reps = count.reps,
+			phenodata.reps = pheno.reps.count
+			);
+		}
 
-	# check CNA concordance
-	conc.matrix <- NanoStringNormCNV::calculate.replicate.concordance(
-		cnas.reps = cna.reps,
-		phenodata.reps = pheno.reps.cnas
-		);
-	conc.summary <- apply(conc.matrix, 2, sum)/nrow(conc.matrix);
-
+	# calculate CNA concordance
+	if (!is.null(cna.reps)) {
+		conc.matrix <- NanoStringNormCNV::calculate.replicate.concordance(
+			cnas.reps = cna.reps,
+			phenodata.reps = pheno.reps.cnas
+			);
+		conc.summary <- apply(conc.matrix, 2, sum)/nrow(conc.matrix);
+		}
+		
 	return(
 		list(
 			variance = var.matrix,
 			concordance = conc.matrix,
 			conc.summary = conc.summary,
-			norm.counts = nano.reps,
-			count.pheno = pheno.reps.nano,
+			norm.counts = count.reps,
+			count.pheno = pheno.reps.count,
 			cna.calls = cna.reps,
 			cna.pheno = pheno.reps.cnas
 			)
