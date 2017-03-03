153eeb09464834c2aa0fb18dbfb9e2a0b732644a
dsendorek <dsendorek@noemail.ucla.edu>
2017-03-03 19:54:34 +0000

    adding data checks to plotting to prevent crashing
    git-svn-id: https://code.boutroslab.oicr.on.ca/svn/BoutrosLab@61467 aa8168d8-c5e5-4185-88a0-f3e152ed764a

--- trunk/NanoStringNormCNV/R/visualize.results.R
+++ trunk/NanoStringNormCNV/R/visualize.results.R
@@ -43,70 +43,103 @@ visualize.results <- function(raw.data, normalized.data, phenodata = NULL, cna.r
 	##############################
 
 	# raw NanoString counts
-	flog.info("Plotting raw counts heatmap..");
-	NanoStringNormCNV::make.counts.heatmap(
-		nano.counts = raw.data.reformatted,
-		fname.stem = 'raw',
-		covs.rows = sample.covs,
-		covs.cols = gene.covs.pre.norm
-		);
+	if (nlevels(as.factor(unlist(raw.data.reformatted))) > 1) {
+		flog.info("Plotting raw counts heatmap..");
+		NanoStringNormCNV::make.counts.heatmap(
+			nano.counts = raw.data.reformatted,
+			fname.stem = 'raw',
+			covs.rows = sample.covs,
+			covs.cols = gene.covs.pre.norm
+			);
+	} else {
+		flog.warn("Unable to plot raw counts data!");
+		}
 
 	# normalized NanoString counts
-	flog.info("Plotting normalized counts heatmap..");
-	NanoStringNormCNV::make.counts.heatmap(
-		nano.counts = normalized.data.reformatted,
-		fname.stem = 'normalized',
-		covs.rows = sample.covs,
-		covs.cols = gene.covs.post.norm
-		);
+	if (nlevels(as.factor(unlist(normalized.data.reformatted))) > 1) {
+		flog.info("Plotting normalized counts heatmap..");
+		NanoStringNormCNV::make.counts.heatmap(
+			nano.counts = normalized.data.reformatted,
+			fname.stem = 'normalized',
+			covs.rows = sample.covs,
+			covs.cols = gene.covs.post.norm
+			);
+	} else {
+		flog.warn("Unable to plot normalized count data!");
+		}
 
 	# raw count correlations
-	flog.info("Plotting raw count correlations heatmap..");
-	NanoStringNormCNV::make.sample.correlations.heatmap(
-		nano.counts = log10(raw.data.reformatted + 1),
-		fname.stem = 'raw-count',
-		covs = sample.covs
-		);
+	if (nlevels(as.factor(unlist(raw.data.reformatted))) > 1) {
+		flog.info("Plotting raw count correlations heatmap..");
+		NanoStringNormCNV::make.sample.correlations.heatmap(
+			nano.counts = log10(raw.data.reformatted + 1),
+			fname.stem = 'raw-count',
+			covs = sample.covs
+			);
+	} else {
+		flog.warn("Unable to plot raw count correlations!");
+		}
 
 	# normalized count correlations
-	flog.info("Plotting normalized count correlations heatmap..");
-	NanoStringNormCNV::make.sample.correlations.heatmap(
-		nano.counts = log10(normalized.data.reformatted + 1),
-		fname.stem = 'norm-count',
-		covs = sample.covs
-		);
+	if (nlevels(as.factor(unlist(normalized.data.reformatted))) > 1) {
+		flog.info("Plotting normalized count correlations heatmap..");
+		NanoStringNormCNV::make.sample.correlations.heatmap(
+			nano.counts = log10(normalized.data.reformatted + 1),
+			fname.stem = 'norm-count',
+			covs = sample.covs
+			);
+	} else {
+		flog.warn("Unable to plot normalized count correlations!");
+		}
 
 	# rounded CNA calls
 	if (! is.null(cna.rounded)){
-		flog.info("Plotting 'rounded CNA calls' heatmap..");
-		NanoStringNormCNV::make.cna.heatmap(
-			nano.cnas = cna.rounded,
-			fname.stem = 'rounded-cna-calls',
-			rounded = TRUE,
-			covs.cols = gene.covs.post.norm,
-			covs.rows = sample.covs,
-			min.cn = 0,
-			width = 7
-			);
+		if (nlevels(as.factor(unlist(cna.rounded))) > 1) {
+			flog.info("Plotting 'rounded CNA calls' heatmap..");
+			NanoStringNormCNV::make.cna.heatmap(
+				nano.cnas = cna.rounded,
+				fname.stem = 'rounded-cna-calls',
+				rounded = TRUE,
+				covs.cols = gene.covs.post.norm,
+				covs.rows = sample.covs,
+				min.cn = 0,
+				width = 7
+				);
+		} else {
+			flog.warn("Unable to plot 'rounded CNA' call data!");
+			}
 		}
 
 	# raw CNA calls
 	if (! is.null(cna.raw)) {
-		flog.info("Plotting 'raw CNA calls' heatmap..");
-		NanoStringNormCNV::make.cna.heatmap(
-			nano.cnas = cna.raw,
-			fname.stem = 'raw-cna-calls',
-			rounded = TRUE,
-			covs.cols = gene.covs.post.norm,
-			covs.rows = sample.covs,
-			min.cn = 0,
-			width = 7
-			);
+		if (nlevels(as.factor(unlist(cna.raw))) > 1) {
+			flog.info("Plotting 'raw CNA calls' heatmap..");
+			NanoStringNormCNV::make.cna.heatmap(
+				nano.cnas = cna.raw,
+				fname.stem = 'raw-cna-calls',
+				rounded = TRUE,
+				covs.cols = gene.covs.post.norm,
+				covs.rows = sample.covs,
+				min.cn = 0,
+				width = 7
+				);
+		} else {
+			flog.warn("Unable to plot 'raw CNA' call data!");
+			}
 		}
 
 	###################################
-	### Density plots (all.samples) ###
+	### Density plots (all samples) ###
 	###################################
+	
+	# rounded CNA calls
+	if (! is.null(cna.rounded)) {
+		flog.info("Plotting 'rounded CNA calls' density plots..");
+		NanoStringNormCNV::make.cna.densities.plots(
+			nano.cnas = cna.rounded,
+			fname.stem = 'rounded-cna-calls'
+			);
+		}
 
 	# raw CNA calls
 	if (! is.null(cna.raw)) {
@@ -117,73 +150,89 @@ visualize.results <- function(raw.data, normalized.data, phenodata = NULL, cna.r
 			);
 		}
 
-	# rounded CNA calls
-	if (! is.null(cna.rounded)) {
-		flog.info("Plotting 'rounded CNA calls' density plots..");
-		NanoStringNormCNV::make.cna.densities.plots(
-			nano.cnas = cna.rounded,
-			fname.stem = 'rounded-cna-calls'
-			);
-		}
-
 	##################################
 	### Heatmaps (replicates only) ###
 	##################################
 
 	if (! is.null(replicate.eval)) {
 		# raw NanoString counts
-		flog.info("Plotting raw counts heatmap for replicates..");
-		NanoStringNormCNV::make.counts.heatmap(
-			nano.counts = replicate.eval$raw.counts,
-			fname.stem = 'replicate_raw',
-			covs.rows = sample.covs,
-			covs.cols = gene.covs.pre.norm,
-			);
+		if (nlevels(as.factor(unlist(replicate.eval$raw.counts))) > 1) {
+			flog.info("Plotting raw counts heatmap for replicates..");
+			NanoStringNormCNV::make.counts.heatmap(
+				nano.counts = replicate.eval$raw.counts,
+				fname.stem = 'replicate_raw',
+				covs.rows = sample.covs,
+				covs.cols = gene.covs.pre.norm,
+				);
+		} else {
+			flog.warn("Unable to plot raw count data for replicates!");
+			}
 		
 		# normalized NanoString counts
-		flog.info("Plotting normalized counts heatmap for replicates..");
-		NanoStringNormCNV::make.counts.heatmap(
-			nano.counts = replicate.eval$norm.counts,
-			fname.stem = 'replicate_norm',
-			covs.cols = gene.covs.post.norm,
-			covs.rows = sample.covs
-			);
-		
+		if (nlevels(as.factor(unlist(replicate.eval$norm.counts))) > 1) {
+			flog.info("Plotting normalized counts heatmap for replicates..");
+			NanoStringNormCNV::make.counts.heatmap(
+				nano.counts = replicate.eval$norm.counts,
+				fname.stem = 'replicate_norm',
+				covs.cols = gene.covs.post.norm,
+				covs.rows = sample.covs
+				);
+		} else {
+			flog.warn("Unable to plot normalized count data for replicates!");
+			}
+
 		# CNA calls
-		flog.info("Plotting CNA calls heatmap for replicates..");
-		NanoStringNormCNV::make.cna.heatmap(
-			nano.cnas = replicate.eval$cna.calls,
-			fname.stem = 'replicate_cna-calls',
-			rounded = TRUE,
-			covs.cols = gene.covs.post.norm,
-			covs.rows = sample.covs,
-			min.cn = 0
-			);
+		if (nlevels(as.factor(unlist(replicate.eval$cna.calls))) > 1) {
+			flog.info("Plotting CNA calls heatmap for replicates..");
+			NanoStringNormCNV::make.cna.heatmap(
+				nano.cnas = replicate.eval$cna.calls,
+				fname.stem = 'replicate_cna-calls',
+				rounded = TRUE,
+				covs.cols = gene.covs.post.norm,
+				covs.rows = sample.covs,
+				min.cn = 0
+				);
+		} else {
+			flog.warn("Unable to plot CNA calls for replicates!");
+			}
+
 
 		# replicate CNA call concordance
-		flog.info("Plotting replicate CNA call concordance heatmap..");
-		NanoStringNormCNV::make.counts.heatmap(
-			nano.counts = replicate.eval$concordance,
-			fname.stem = 'replicate_cna-concordance',
-			covs.cols = gene.covs.post.norm,
-			print.ylab = TRUE
-			);
+		if (nlevels(as.factor(unlist(replicate.eval$concordance))) > 1) {
+			flog.info("Plotting replicate CNA call concordance heatmap..");
+			NanoStringNormCNV::make.counts.heatmap(
+				nano.counts = replicate.eval$concordance,
+				fname.stem = 'replicate_cna-concordance',
+				covs.cols = gene.covs.post.norm,
+				print.ylab = TRUE
+				);
+		} else {
+			flog.warn("Unable to plot CNA call concordance for replicates!");
+			}
 
 		# normalized count correlations
-		flog.info("Plotting normalized count correlations heatmap for replicates..");
-		NanoStringNormCNV::make.sample.correlations.heatmap(
-			nano.counts = log10(replicate.eval$norm.counts + 1),
-			fname.stem = 'replicate_norm-count',
-			covs = sample.covs
-			);
-
+		if (nlevels(as.factor(unlist(replicate.eval$norm.counts))) > 1) {
+			flog.info("Plotting normalized count correlations heatmap for replicates..");
+			NanoStringNormCNV::make.sample.correlations.heatmap(
+				nano.counts = log10(replicate.eval$norm.counts + 1),
+				fname.stem = 'replicate_norm-count',
+				covs = sample.covs
+				);
+		} else {
+			flog.warn("Unable to plot normalized count correlations for replicates!");
+			}
 
 		# normalized count correlations (tumour only)
-		flog.info("Plotting normalized count correlations heatmap for replicates (tumour only)..");
-		NanoStringNormCNV::make.sample.correlations.heatmap(
-			nano.counts = log10(replicate.eval$norm.counts[, which(replicate.eval$count.pheno$Type == 'Tumour')] + 1),
-			fname.stem = 'replicate_tumour-only_norm-count',
-			covs = sample.covs
-			);
+		norm.counts.tumour.only <- replicate.eval$norm.counts[, which(replicate.eval$count.pheno$Type == 'Tumour')];
+		if (nlevels(as.factor(unlist(norm.counts.tumour.only))) > 1) {
+			flog.info("Plotting normalized count correlations heatmap for replicates (tumour only)..");
+			NanoStringNormCNV::make.sample.correlations.heatmap(
+				nano.counts = log10(norm.counts.tumour.only + 1),
+				fname.stem = 'replicate_tumour-only_norm-count',
+				covs = sample.covs
+				);
+		} else {
+			flog.warn("Unable to plot normalized count correlations for tumour only replicates!");
+			}
 		}
 	}
