e6a88be37517b1384cdde67f13347ba73464be3f
dsendorek <dsendorek@noemail.ucla.edu>
2017-03-09 16:44:14 +0000

    modifying collapse.genes so only non-control probes get collapsed (i.e. Endogenous, Housekeeping); incl info in man
    git-svn-id: https://code.boutroslab.oicr.on.ca/svn/BoutrosLab@61544 aa8168d8-c5e5-4185-88a0-f3e152ed764a

--- trunk/NanoStringNormCNV/R/collapse.genes.R
+++ trunk/NanoStringNormCNV/R/collapse.genes.R
@@ -1,23 +1,31 @@
 collapse.genes <- function(normalized.data){
-	# generate normalized.data.aggr gene-level data
+	# split control and non-control probes
+	normalized.data.non.ctrl <- normalized.data[ normalized.data$CodeClass %in% c('Endogenous', 'Housekeeping'),];
+	normalized.data.ctrl     <- normalized.data[!normalized.data$CodeClass %in% c('Endogenous', 'Housekeeping'),];
+
+	# generate normalized.data.aggr gene-level data for non-control probes
 	normalized.data.gene <- aggregate(
-			x = normalized.data[, !colnames(normalized.data) %in% c('CodeClass', 'Name', 'Accession')],
-			by = list(normalized.data$Accession),
-			FUN = mean
-			);
+		x = normalized.data.non.ctrl[, !colnames(normalized.data.non.ctrl) %in% c('CodeClass', 'Name', 'Accession')],
+		by = list(normalized.data.non.ctrl$Accession),
+		FUN = mean
+		);
 
 	# rename the Group.1 column
 	colnames(normalized.data.gene)['Group.1' == colnames(normalized.data.gene)] <- 'Name';
 
 	# add gene annotation 
-	matching.inds <- unlist(lapply(normalized.data.gene$Name, function(f) which(normalized.data$Accession == f)[1]));
+	matching.inds <- unlist(lapply(normalized.data.gene$Name, function(f) which(normalized.data.non.ctrl$Accession == f)[1]));
 	normalized.data.gene  <- cbind(
-		CodeClass = normalized.data[matching.inds, "CodeClass"],
+		CodeClass = normalized.data.non.ctrl[matching.inds, "CodeClass"],
 		Name = normalized.data.gene[, "Name"],
-		Accession = normalized.data[matching.inds, "Accession"],
-		normalized.data.gene[, !colnames(normalized.data.gene) == "Name"]
+		Accession = normalized.data.non.ctrl[matching.inds, "Accession"],
+		normalized.data.gene[, !colnames(normalized.data.gene) == "Name"],
+		stringsAsFactors = FALSE
 		);
 	rownames(normalized.data.gene) <- normalized.data.gene$Name;
+
+	# re-combine with (non-aggregated) control probes
+	normalized.data.gene <- rbind(normalized.data.gene, normalized.data.ctrl);
 
 	return(normalized.data.gene);
 	}
