87b6a83d4d30564a048f554e9e345f39a7c9cdd9
dsendorek <dsendorek@noemail.ucla.edu>
2017-03-20 21:09:42 +0000

    small changes to doc/comments, etc; removing norm of POS, NEG, and RestrictionSite probes --not sure why this was done in the first place, reverting back to manual specs
    git-svn-id: https://code.boutroslab.oicr.on.ca/svn/BoutrosLab@61836 aa8168d8-c5e5-4185-88a0-f3e152ed764a

--- trunk/NanoStringNormCNV/R/invariant.probe.norm.R
+++ trunk/NanoStringNormCNV/R/invariant.probe.norm.R
@@ -1,9 +1,6 @@
-invariant.probe.norm <- function(normalized.data, phenodata = NULL){
-	# NB: it is not necessary to normalize the data for POS, NEG or Restriction Site controls
-	# INV-normalization procedure should be carried out on the Inv and End code classes.
-
+invariant.probe.norm <- function(nanostring.data, phenodata = NULL){
 	# remove CodeClass, Name, Accession from colnames
-	grep.cols <- colnames(normalized.data)[!colnames(normalized.data) %in% c('CodeClass', 'Name', 'Accession')];
+	grep.cols <- colnames(nanostring.data)[!colnames(nanostring.data) %in% c('CodeClass', 'Name', 'Accession')];
 
 	# check if sample ID and tissue type info exists
 	if (!is.null(phenodata)) {
@@ -12,47 +9,40 @@ invariant.probe.norm <- function(normalized.data, phenodata = NULL){
 		phenodata <- NULL;
 		}
 
-	# step 1
-	# get average values for all the invariant probes 
+	# get average values for all the invariant probes, per sample
 	inv.avg <- apply(
-		X = normalized.data['Invariant' == normalized.data$CodeClass, grep.cols, drop = FALSE],
+		X = nanostring.data['Invariant' == nanostring.data$CodeClass, grep.cols, drop = FALSE],
 		MARGIN = 2,
 		FUN = mean
 		);
 
-	# step 2A
 	# calculate mean of average INV count values
 	mean.inv.avg <- mean(inv.avg);
 
-	# step 2B
-	# plot the counts for the invariant probes. It is BAD to have counts < 100 for these probes as it signifies
-	# low DNA input, leading to unreliable CNA calls. Especially if the low counts are in the reference samples!!
+	# plot counts of invariant probes: counts of < 100 signify low DNA input, leading to unreliable CNA calls
+	# this is especially problematic if the low counts are in the reference samples!
 	NanoStringNormCNV::make.invariant.probe.plot(
-		inv.probe.counts = normalized.data['Invariant' == normalized.data$CodeClass, grep.cols, drop = FALSE],
+		inv.probe.counts = nanostring.data['Invariant' == nanostring.data$CodeClass, grep.cols, drop = FALSE],
 		tissue.type = phenodata
 		);
 
-	# step 3
 	# calculate normalization factor
 	norm.factor <- mean.inv.avg / inv.avg;
 
-	# create a new object for nano.norm
-	rows.to.norm <- 1:nrow(normalized.data);
-	nano.norm    <- normalized.data[rows.to.norm, c('CodeClass', 'Name', 'Accession')];
-	nano.norm[,grep.cols] <- NA;
+	# create a new object where 'Positive', 'Negative' and 'RestrictionSite' probes will not be normalized
+	rows.to.norm <- !(nanostring.data$CodeClass %in% c('Positive', 'Negative', 'RestrictionSite'));
+	nano.norm    <- nanostring.data[, c('CodeClass', 'Name', 'Accession', grep.cols)];
+	nano.norm[rows.to.norm, grep.cols] <- NA;
 
 	# sanity check to make sure all names are the same
-	if (!all(names(norm.factor) == grep.cols)) { stop ('Mismatch in Names!'); }
+	if (!all(names(norm.factor) == grep.cols)) { stop('Mismatch in Names!'); }
 
-	# loop over each sample
+	# get INV-normalized counts (counts for each probe x normalization factor) per sample
 	for (ind in 1:length(grep.cols)) {
-
-		# get the ind-th row
-		this.col    <- normalized.data[rows.to.norm, grep.cols[ind], drop = FALSE];
+		this.col    <- nanostring.data[rows.to.norm, grep.cols[ind], drop = FALSE];
 		this.factor <- norm.factor[ind];
 
-		# get INV-normalized counts (RAW counts for each probe x normalization factor)
-		nano.norm[,grep.cols[ind]] <- this.col * this.factor;
+		nano.norm[rows.to.norm, grep.cols[ind]] <- this.col * this.factor;
 		}
 
 	return(nano.norm);
