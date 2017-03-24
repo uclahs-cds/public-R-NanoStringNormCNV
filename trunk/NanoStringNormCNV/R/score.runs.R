f7b2050f720b093d4f62c1c1cb97581a0b6bfd5d
dsendorek <dsendorek@noemail.ucla.edu>
2017-03-24 19:58:44 +0000

    checking for negative values before logging in case user chooses not to transform data post norm
    git-svn-id: https://code.boutroslab.oicr.on.ca/svn/BoutrosLab@62041 aa8168d8-c5e5-4185-88a0-f3e152ed764a

--- trunk/NanoStringNormCNV/R/score.runs.R
+++ trunk/NanoStringNormCNV/R/score.runs.R
@@ -23,11 +23,18 @@ score.runs <- function(replicate.eval, normalized.data, cna.rounded, phenodata, 
 	# order annotation
 	phenodata <- phenodata[match(colnames(normalized.data[, -(1:3)]), phenodata$SampleID),];
 
+	# log normalized counts, if possible
+	if (all(unlist(normalized.data) >= 0)) {
+		norm.data.clust <- log10(normalized.data[, -c(1:3)] + 1);
+	} else {
+		norm.data.clust <- normalized.data;
+		}
+
 	### Check the adjusted rand index (ARI) of multiple parameters
 	# evaluate using cartridge information (all patients)
 	flog.info("Calculating ARI using cartridge information (all samples)..");
 	scores$ari.chip <- NanoStringNormCNV::get.ari(
-		data.to.cluster = log10(normalized.data[, -c(1:3)] + 1),
+		data.to.cluster = norm.data.clust,
 		feature = phenodata$Cartridge,
 		is.discrete = FALSE
 		);
@@ -42,9 +49,16 @@ score.runs <- function(replicate.eval, normalized.data, cna.rounded, phenodata, 
 			match(colnames(replicate.eval$norm.counts), phenodata$SampleID)
 			,]$Patient;
 		
+		# log normalized counts if possible
+		if (all(unlist(replicate.eval$norm.counts) >= 0)) {
+			rep.norm.data.clust <- log10(replicate.eval$norm.counts + 1);
+		} else {
+			rep.norm.data.clust <- replicate.eval$norm.counts;
+			}
+
 		flog.info("Calculating ARI using normalized counts (tumour only)..");
 		scores$ari.pts.normcor <- NanoStringNormCNV::get.ari(
-			data.to.cluster = log10(replicate.eval$norm.counts + 1),
+			data.to.cluster = rep.norm.data.clust,
 			feature = replicate.eval$count.pheno$Patient[replicate.eval$count.pheno$Type == 'Tumour'],
 			is.discrete = FALSE
 			);
@@ -55,7 +69,7 @@ score.runs <- function(replicate.eval, normalized.data, cna.rounded, phenodata, 
 	if (any(phenodata$Type == 'Reference')) {
 		flog.info("Calculating ARI using tissue type information (all samples)..");
 		scores$ari.type <- NanoStringNormCNV::get.ari(
-			data.to.cluster = log10(normalized.data[, -c(1:3)] + 1),
+			data.to.cluster = norm.data.clust,
 			feature = phenodata$Type,
 			is.discrete = FALSE
 			);
