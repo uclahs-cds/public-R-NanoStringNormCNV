3a0ed72ba7ef0883cc733372b466cab2b90ad407
dsendorek <dsendorek@noemail.ucla.edu>
2017-03-29 16:37:36 +0000

    creating new tag
    git-svn-id: https://code.boutroslab.oicr.on.ca/svn/BoutrosLab@62190 aa8168d8-c5e5-4185-88a0-f3e152ed764a

---
+++ tags/NanoStringNormCNV-1.0.1/R/get.cna.thresholds.R
@@ -0,0 +1,24 @@
+get.cna.thresholds <- function(ratios, percent = 0.8) {
+	# perform data check
+	if (length(ratios) < 2) {
+		stop("You must specify a data vector of length > 1");
+		} 
+
+	# calculate thresholds based on a kernel density method
+    dx <- density(x = ratios, kernel = 'gaussian', na.rm = TRUE);
+    dn <- cumsum(dx$y) / sum(dx$y);
+    li <- which(dn >= (1 - percent) / 2)[1];
+    ui <- which(dn >= 1 - (1 - percent) / 2)[1];
+
+    thresholds <- c(
+    	lower <- dx$x[li],
+    	upper <- dx$x[ui]
+    	);
+
+	thresh.df <- data.frame(
+		Loss = thresholds[1], 
+		Gain = thresholds[2]
+		);  
+
+	return(thresh.df);
+	}
