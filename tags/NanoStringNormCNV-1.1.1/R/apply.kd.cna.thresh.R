3adcccf99ae18074e1751bb10fd7708af8508d5e
dsendorek <dsendorek@noemail.ucla.edu>
2017-09-29 16:55:35 +0000

    creating new tag
    git-svn-id: https://code.boutroslab.oicr.on.ca/svn/BoutrosLab@66446 aa8168d8-c5e5-4185-88a0-f3e152ed764a

---
+++ tags/NanoStringNormCNV-1.1.1/R/apply.kd.cna.thresh.R
@@ -0,0 +1,62 @@
+apply.kd.cna.thresh <- function(ratio.data, kd.values, neutral.cn = 2) {
+	if (2 != length(kd.values) & 4 != length(kd.values)) {
+		stop("Must provide either two or four KD values! Please see documentation for details.");
+		}
+
+	# assign header names
+	headers <- c('Code.Class', 'CodeClass', 'Name', 'Accession');
+
+	# define sample columns
+	which.cna <- colnames(ratio.data)[!colnames(ratio.data) %in% headers];
+	which.n   <- which(colnames(ratio.data) %in% which.cna);
+
+	# need to exclude columns with all NAs:
+	# occurs when perchip = T and there are no reference samples on that chip!
+	na.counts <- apply(
+		X = ratio.data[,which.n, drop = FALSE],
+		MARGIN = 2,
+		FUN = function(f) { all(is.na(f)) }
+		);
+
+	if (any(na.counts)) {
+		all.na <- which(na.counts);
+		which.n <- which.n[-all.na];
+		which.cna <- which.cna[which.n];
+		}
+	cna.output <- ratio.data[, which.cna, drop = FALSE];
+		
+	# determine the thresholds based on all patients combined
+	# shown to be more stable if only considering small subset of patients
+	if (2 == length(kd.values)) {
+
+		cna.thresh.single <- NanoStringNormCNV:::get.cna.thresholds(ratios = unlist(cna.output), percent = kd.values[1]); # het
+		cna.thresh.multi  <- NanoStringNormCNV:::get.cna.thresholds(ratios = unlist(cna.output), percent = kd.values[2]); # hom
+		thresh <- c(cna.thresh.multi[1], cna.thresh.single, cna.thresh.multi[2]);
+
+	} else if (4 == length(kd.values)) {
+
+		thresh <- vector(length = 4);
+		thresh[1] <- NanoStringNormCNV:::get.cna.thresholds(ratios = unlist(cna.output), percent = kd.values[1])[[1]]; # hom del
+		thresh[2] <- NanoStringNormCNV:::get.cna.thresholds(ratios = unlist(cna.output), percent = kd.values[2])[[1]]; # het del
+		thresh[4] <- NanoStringNormCNV:::get.cna.thresholds(ratios = unlist(cna.output), percent = kd.values[4])[[2]]; # hom gain
+		thresh[3] <- NanoStringNormCNV:::get.cna.thresholds(ratios = unlist(cna.output), percent = kd.values[3])[[2]]; # het gain
+
+		}
+
+	# loop over each sample to call CNA states
+	for (col.ind in 1:ncol(cna.output)) {
+		cna.output[, col.ind] <- NanoStringNormCNV:::tumour.normal.ratio.to.cn.state(
+			ratios = cna.output[, col.ind],
+			thresholds = thresh
+			);
+		}
+
+	# sum CNA state and neutral CN to get observed CN
+	cna.output <- cna.output + neutral.cn;
+
+	# set any negative CN to zero
+	# this occurs when a male sex chromosome segment is called as having a CNA state of -2
+	cna.output[cna.output < 0 & !is.na(cna.output)] <- 0;
+
+	return(cna.output);
+	}
---
+++ tags/NanoStringNormCNV-1.1.1/R/make.sample.correlations.heatmap.R
@@ -0,0 +1,57 @@
+make.sample.correlations.heatmap <- function(nano.counts, cor.method = 'pearson', fname.stem = NULL, covs = NULL) {
+	# set up legend
+	if (!is.null(covs)) {
+		# get required samples
+		covs <- covs[covs$SampleID %in% names(nano.counts),];
+
+		# remove 'Type' if all samples are either 'Tumour' or 'Reference'
+		if ("Type" %in% colnames(covs)) {
+			if (nlevels(as.factor(covs$Type)) == 1) {
+				covs <- covs[, !colnames(covs) %in% "Type", drop = FALSE];
+				}
+			}
+
+		# covariates
+		cov.objs <- NanoStringNormCNV:::generate.plot.covariates(
+			plotting.data = nano.counts,
+			sample.covariates = covs
+			);
+		cov.obj <- cov.objs[['sample']];
+
+		# legend
+		covs <- covs[, names(covs) != 'SampleID', drop = FALSE];
+		covs.legend <- NanoStringNormCNV:::generate.plot.legend(cov.info = as.list(covs));
+	} else {
+		covs.legend <- NULL;
+		cov.obj <- NULL;
+		}
+
+	# set up file name
+	if (!is.null(fname.stem)) { fname.stem <- paste0("_", fname.stem); }
+
+	# get the inter-array correlations
+	correlations <- cor(x = nano.counts, use = 'all.obs', method = cor.method);
+
+	# plot
+	cov.border <- list(col = 'black', lwd = 1.5);
+	BoutrosLab.plotting.general::create.heatmap(
+		x = correlations,
+		filename = paste0(Sys.Date(), fname.stem, '_inter-sample-correlation-heatmap.tiff'),
+		clustering.method = 'complete',
+		plot.dendrograms = 'both',
+		xaxis.rot = 90,
+		xaxis.tck = 0,
+		yaxis.tck = 0,
+		covariates = cov.obj,
+		covariates.top.grid.border = cov.border,
+		covariates.top = cov.obj,
+		covariates.grid.border = cov.border,
+		covariate.legends = covs.legend,
+		legend.title.just = 'left',
+		legend.title.fontface = 'bold',
+		scale.data = FALSE,
+		colourkey.cex = 2.0,
+		resolution = 600,
+		width = 7
+		);
+	}
