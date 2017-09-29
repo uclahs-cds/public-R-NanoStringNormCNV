50b65805adb7f7ad0fa7ec9f793b3f2147e21333
dsendorek <dsendorek@noemail.ucla.edu>
2017-09-29 15:40:13 +0000

    updating manual with Vince's suggestions
    git-svn-id: https://code.boutroslab.oicr.on.ca/svn/BoutrosLab@66429 aa8168d8-c5e5-4185-88a0-f3e152ed764a

--- trunk/NanoStringNormCNV/inst/doc/NanoStringNormCNV_Introduction.R
+++ trunk/NanoStringNormCNV/inst/doc/NanoStringNormCNV_Introduction.R
@@ -58,9 +58,9 @@ print(head(PhenoData));
 
 
 ###################################################
-### code chunk number 6: eg.restriction.fragmentation.control.qc (eval = FALSE)
+### code chunk number 6: eg.restriction.fragmentation.control.qc.alui (eval = FALSE)
 ###################################################
-## # checking AluI-digested samples only
+## # correctly running QC on AluI-digested samples only
 ## excl.samples <- PhenoData$SampleID[PhenoData$Fragmentation != "AluI"];
 ## probe.ratios <- restriction.fragmentation.qc(
 ## 	raw.data = NanoString[, ! names(NanoString) %in% excl.samples]
@@ -68,7 +68,16 @@ print(head(PhenoData));
 
 
 ###################################################
-### code chunk number 7: eg.invariant.control.qc (eval = FALSE)
+### code chunk number 7: eg.restriction.fragmentation.control.qc.all (eval = FALSE)
+###################################################
+## # running QC on all available samples (\textit{i.e.} AluI-digested and sonicated)
+## probe.ratios <- restriction.fragmentation.qc(
+## 	raw.data = NanoString
+## 	);
+
+
+###################################################
+### code chunk number 8: eg.invariant.control.qc (eval = FALSE)
 ###################################################
 ## # plotting invariant probes
 ## make.invariant.probe.plot(
@@ -78,7 +87,7 @@ print(head(PhenoData));
 
 
 ###################################################
-### code chunk number 8: eg.remove.low.qual
+### code chunk number 9: eg.remove.low.qual
 ###################################################
 low.quality <- c('CPCG0266B.M1', 'CPCG0248B.M2');
 NanoString  <- NanoString[, !names(NanoString) %in% low.quality];
@@ -86,7 +95,7 @@ PhenoData   <- PhenoData[!PhenoData$SampleID %in% low.quality,];
 
 
 ###################################################
-### code chunk number 9: eg.update.data
+### code chunk number 10: eg.update.data
 ###################################################
 # update matched normal and replicate information, as necessary
 PhenoData[PhenoData$SampleID == 'CPCG0248F1',]$ReferenceID <- 'missing';
@@ -94,7 +103,7 @@ PhenoData[PhenoData$SampleID %in% c('CPCG0266B.M2', 'CPCG0248B.M1'),]$HasReplica
 
 
 ###################################################
-### code chunk number 10: eg.norm1
+### code chunk number 11: eg.norm1
 ###################################################
 # example 1
 # perform invariant probe normalization only --cartridges combined
@@ -111,7 +120,7 @@ NanoString.norm <- normalize.global(
 
 
 ###################################################
-### code chunk number 11: eg.norm2
+### code chunk number 12: eg.norm2
 ###################################################
 # example 2
 # perform invariant probe normalization only --cartridges individually
@@ -128,7 +137,7 @@ NanoString.norm <- normalize.per.chip(
 
 
 ###################################################
-### code chunk number 12: eg.norm3
+### code chunk number 13: eg.norm3
 ###################################################
 # example 3
 # include covariates for sample cartridge and sample type 
@@ -162,7 +171,7 @@ NanoString.norm <- normalize.global(
 
 
 ###################################################
-### code chunk number 13: eg.norm4
+### code chunk number 14: eg.norm4
 ###################################################
 # same as above but per chip
 NanoString.norm <- normalize.per.chip(
@@ -178,14 +187,14 @@ NanoString.norm <- normalize.per.chip(
 
 
 ###################################################
-### code chunk number 14: eg.collapse.genes
+### code chunk number 15: eg.collapse.genes
 ###################################################
 NanoString.norm.col <- collapse.genes(normalized.data = NanoString.norm);
 print(NanoString.norm.col[1:6, 1:6]);
 
 
 ###################################################
-### code chunk number 15: call.cnas.matched.ref
+### code chunk number 16: call.cnas.matched.ref
 ###################################################
 # Option 1: call using matched normal reference
 cnas <- call.cnas.with.matched.normals(
@@ -199,7 +208,7 @@ cnas <- call.cnas.with.matched.normals(
 
 
 ###################################################
-### code chunk number 16: call.cnas.pooled.ref
+### code chunk number 17: call.cnas.pooled.ref
 ###################################################
 # Option 2: call using a pooled normals reference
 cnas <- call.cnas.with.pooled.normals(
@@ -220,7 +229,7 @@ cnas <- call.cnas.with.pooled.normals(
 
 
 ###################################################
-### code chunk number 17: eg.eval.reps
+### code chunk number 18: eg.eval.reps
 ###################################################
 # if technical replicates are available
 evaluation <- evaluate.replicates(
@@ -231,7 +240,7 @@ evaluation <- evaluate.replicates(
 
 
 ###################################################
-### code chunk number 18: eg.ari1
+### code chunk number 19: eg.ari1
 ###################################################
 # how well does the data cluster around the patients from which samples were obtained
 patient.ari <- get.ari(
@@ -242,7 +251,7 @@ patient.ari <- get.ari(
 
 
 ###################################################
-### code chunk number 19: eg.ari2
+### code chunk number 20: eg.ari2
 ###################################################
 # how much does the data cluster around the cartridges on which the samples were processed
 # log values, if appropriate
@@ -260,7 +269,7 @@ cartridge.ari <- get.ari(
 
 
 ###################################################
-### code chunk number 20: eg.vis1 (eval = FALSE)
+### code chunk number 21: eg.vis1a (eval = FALSE)
 ###################################################
 ## # plot normalized NanoString counts
 ## make.counts.heatmap(
@@ -269,7 +278,11 @@ cartridge.ari <- get.ari(
 ## 	covs.rows = PhenoData[, c('SampleID', 'Type', 'Cartridge')],
 ## 	covs.cols = NanoString[, c('Name', 'CodeClass')]
 ## 	);
-## 
+
+
+###################################################
+### code chunk number 22: eg.vis1b (eval = FALSE)
+###################################################
 ## # plot raw NanoString counts
 ## # make sure raw count data frame has gene names for row names!
 ## NanoString.formatted <- NanoString[, -(1:3)];
@@ -284,7 +297,7 @@ cartridge.ari <- get.ari(
 
 
 ###################################################
-### code chunk number 21: eg.vis2 (eval = FALSE)
+### code chunk number 23: eg.vis2a (eval = FALSE)
 ###################################################
 ## # plot rounded copy number calls
 ## make.cna.heatmap(
@@ -294,10 +307,18 @@ cartridge.ari <- get.ari(
 ## 	covs.cols = NanoString[, c('Name', 'CodeClass')],
 ## 	rounded = TRUE
 ## 	);
-## 
+
+
+###################################################
+### code chunk number 24: eg.vis2b (eval = FALSE)
+###################################################
 ## # plot raw (not rounded) copy number calls
+## # first, setting max copy number value at 5
+## cnas.raw.max5 <- cnas$raw;
+## cnas.raw.max5[cnas.raw.max5 > 5] <- 5;
+## 
 ## make.cna.heatmap(
-## 	nano.cnas = cnas$raw,
+## 	nano.cnas = cnas.raw.max5,
 ## 	fname.stem = 'raw',
 ## 	covs.rows = PhenoData[, c('SampleID', 'Type', 'Cartridge')],
 ## 	covs.cols = NanoString[, c('Name', 'CodeClass')],
@@ -306,7 +327,7 @@ cartridge.ari <- get.ari(
 
 
 ###################################################
-### code chunk number 22: eg.vis3 (eval = FALSE)
+### code chunk number 25: eg.vis3 (eval = FALSE)
 ###################################################
 ## # plot copy number call density for rounded values
 ## # two plots: per gene and per sample
@@ -316,7 +337,7 @@ cartridge.ari <- get.ari(
 
 
 ###################################################
-### code chunk number 23: eg.vis4 (eval = FALSE)
+### code chunk number 26: eg.vis4 (eval = FALSE)
 ###################################################
 ## # plot raw NanoString count correlations
 ## make.sample.correlations.heatmap(
@@ -326,9 +347,9 @@ cartridge.ari <- get.ari(
 
 
 ###################################################
-### code chunk number 24: eg.vis5 (eval = FALSE)
+### code chunk number 27: eg.vis5 (eval = FALSE)
 ###################################################
-## # plot all results using wrapper function
+## # alternatively, plot all results using wrapper function
 ## visualize.results(
 ##     raw.data = NanoString,
 ##     normalized.data = NanoString.norm,
